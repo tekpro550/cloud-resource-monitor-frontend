9984b8ae83b7db3fd90ad293e5ed276555b46486
Your Name <your.email@example.com>
2025-06-15 11:04:08 +0530

    Fix ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['react-app'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}; 
